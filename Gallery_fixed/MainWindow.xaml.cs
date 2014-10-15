d53111bec926c33ad58f145e17317e21349e54fa
Group2011 <wandatsy.app@gmail.com>
2014-10-15 09:29:47 +0300

    First upload

---
+++ Gallery_fixed/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gallery
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool logged = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            Login.Visibility = System.Windows.Visibility.Visible;
+            Register.Visibility = System.Windows.Visibility.Visible;
+            RegisterLabel.Visibility = System.Windows.Visibility.Collapsed;
+            RegisterAction.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void Login_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            RegisterLabel.Visibility = System.Windows.Visibility.Visible;
+            RegisterAction.Visibility = System.Windows.Visibility.Collapsed;
+            Register.Visibility = System.Windows.Visibility.Hidden;
+            Login.Visibility = System.Windows.Visibility.Visible;
+            logged = true;
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (logged)
+            {
+                tbSearch.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+    }
+}
