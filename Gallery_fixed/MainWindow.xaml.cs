517c597b644334e77efd3e9b951a321dd18d2947
Group2011 <wandatsy.app@gmail.com>
2014-10-15 10:05:54 +0300

    parsing method added

--- Gallery_fixed/MainWindow.xaml.cs
+++ Gallery_fixed/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HtmlAgilityPack;
+using System.Net;
 
 namespace Gallery
 {
@@ -43,12 +45,64 @@ namespace Gallery
             logged = true;
         }
 
-        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        /*private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
             if (logged)
             {
                 tbSearch.Visibility = System.Windows.Visibility.Visible;
             }
+        }*/
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e) // метод нужно отладить
+        {
+            string url = @"http://pikabu.ru/story/fotografu_na_zametku_2743001";
+            WebClient wc = new WebClient();
+            wc.Proxy = new WebProxy("10.3.0.3", 3128);
+            wc.Proxy.Credentials = new NetworkCredential("inet", "netnetnet");
+
+            try
+            {
+                wc.DownloadFile(url, "1.html");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при попытке парсинга страницы\n" + url);
+            }
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load("1.html", Encoding.UTF8);
+            
+            HtmlNodeCollection images = doc.DocumentNode.SelectNodes("//img");
+            int errors = 0;
+
+            foreach (HtmlNode n in images)
+            {
+                try
+                {
+                    string imgPath = n.Attributes["src"].Value;
+                    if (!imgPath.Contains("//")) // Если урл не содержит двух слешей, значит адрес изображения относительный и нужно вычленить из url домен сайта
+                    {
+                        int c = 0;
+                        for (int i=0; i<url.Length; i++)
+                        {
+                            if (url[i] == '/')
+                                c++;
+                            if (c == 3)
+                            {
+                                imgPath = url.Substring(0, i + 1) + imgPath;
+                                break;
+                            }
+                        }
+                    }
+                    
+                    wc.DownloadFile(imgPath, @"..\..\Images\" + n.Attributes["src"].Value.Substring(n.Attributes["src"].Value.LastIndexOf('/')));
+                }
+                catch 
+                {
+                    errors++;
+                }
+            }
+            MessageBox.Show(errors.ToString());
         }
     }
 }
